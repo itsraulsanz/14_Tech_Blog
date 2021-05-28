7770a11c1d5b04c56c290294953cfa514e39ac3c
Raul Sanz <luar_znas@hotmail.com>
2021-05-28 14:49:19 +0100

    Configurate the homepage and fixed the CSS link problem

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,51 +1,58 @@
-const router = require('express').Router();
-const { Project, User } = require('../models');
+const router = require("express").Router();
+const { Post, User } = require("../models");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+
+    console.log("Hitting get all posts")
   try {
     // Get all posts and JOIN with user data
     const postData = await Post.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
-
+        console.log(postData)
     // Serialize data so the template can read it
     const post = postData.map((post) => post.get({ plain: true }));
-
+    console.log("post-mapping", post)
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      post, 
-      logged_in: req.session.logged_in 
-    });
+    res.render("post", {post, logged_in: req.session.logged_in});
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/project/:id', async (req, res) => {
+router.get("/post/:id", async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
 
     const post = postData.get({ plain: true });
 
-    res.render('post', {
+    res.render("post", {
       ...post,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+router.get("/signup", (req, res) => {
+    res.render('signup')
+})
+
+
+router.get("/login", (req, res) => {
+    res.render('login')
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const path = require("path")
 const routes = require("./controllers");
 
 const sequelize = require("./config/connection");
@@ -28,6 +29,7 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(routes);
 
